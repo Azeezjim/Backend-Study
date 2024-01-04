7c757097835a08eb3a74a9b5219c6f8abae46877
Azeezjim <azeexjim@gmail.com>
2024-01-04 02:32:36 +0100

    learning backend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http')
 
-const saver = http.createServer((req, res )=> {
+const saver = http.createServer((req, res) => {
   console.log(req.url, req.headers, req.method)
   const url = req.url
   if (url === '/') {
@@ -12,9 +12,9 @@ const saver = http.createServer((req, res )=> {
   }
   // process.emit()
   res.write('<html>')
-    res.write('<head><hi>Welcome Page</hi></head>')
-    res.write('<body><form action="/message" action="POST"><input test="text" name="message" ><button type="submit"></button></form></body>')
-    res.write('</html>')
-     res.end
+  res.write('<head><hi>Welcome Page</hi></head>')
+  res.write('<body><h1>Welcome To The Welcome Page</h1></body>')
+  res.write('</html>')
+  res.end
 })
-saver.listen(5005, () =>  console.log("listening..."))
+saver.listen(5005, () => console.log("listening..."))
