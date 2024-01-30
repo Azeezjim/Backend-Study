65e5776da63eb3ea5d702145fa980a44715c39eb
Azeezjim <azeexjim@gmail.com>
2024-01-30 17:38:52 +0100

    Learning back end commit

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 
 const express = require("express");
 
-const  bodyParser = require('body-parser')
+// const  bodyParser = require('body-parser')
 
 const app = express();
 
-app.use{bodyParser.urlencoded()}
+// app.use{bodyParser.urlencoded()}
 
 app.use("/add-prodect", (req, res, next) => {
-  res.send(<form action="/prodoct" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></input></form>)
+  res.send('<form action="/prodoct" method="POST"><input type="text" name="title"> <button type="submit">Add Product</button></input></form>')
 })
 
 app.use('/prodoct', ( req, res, next) => {
-  console.log(res.body);
+  console.log(req.body);
   res.redirect("/");
 })
 
 app.use('/', (req, res, next) => {
-  console.log("Second middleware");
+  // console.log("Second middleware");
   res.send("Hello World");
 })
 
